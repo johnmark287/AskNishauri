2eceb5f18b4d4e51e34eaec496eea6cf01d7fd17
johnmark287 <johhnmark.muhando@students.jkuat.ac.ke>
2024-05-07 17:28:09 +0300

    styles: button update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,9 +5,12 @@ function Sidebar() {
       <div className="text-left m-1 p-[6px] relative">
         AskNishauri
       </div>
-      <button type="button" className="bg-[rgba(72,81,89,255)] rounded-xl w-[90%] self-center text-white">
+      <button type="button" className="hover:bg-[rgba(131,142,152,255)] active:bg-[rgba(29,31,34,255)] my-2 py-[7px] bg-[rgba(72,81,89,255)] rounded-3xl w-[90%] self-center text-white">
         Create chat
       </button>
+      <div className="m-2">
+        History
+      </div>
     </aside>
   )
 }

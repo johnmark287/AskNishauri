9248c8e8c6dc334dd201cb5c59bdcc6b91110d99
johnmark287 <johhnmark.muhando@students.jkuat.ac.ke>
2024-05-07 18:56:54 +0300

    feat: add navbar components and icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 function Navbar() {
   return (
-    <div className='bg-[rgba(29,31,34,255)] border-b m-1 p-2'>
-        Navbar
+    <div className='relative flex justify-between bg-[hsl(216,8%,12%)] border-b border-gray-700 p-3 te'>
+        <div className="bg-[rgba(35,38,43,255)] rounded-2xl flex justify-between">
+          <div className="my-1 mx-4 text-[#87929a]">
+            AskNishauri
+          </div>
+          <button className="my-1 ml-3">
+            <svg className="text-[#87929a] fill-current" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-345 240-585l56-56 184 184 184-184 56 56-240 240Z"/></svg>
+          </button>
+        </div>
+        <div className="flex justify-between">
+          <button type="button" className="px-2 mx-1 text-[rgba(37,188,134,255)] rounded-2xl bg-[rgba(30,48,44,255)]">
+            Export chat
+          </button>
+          <button type="button" className="mx-1">
+            <svg className="fill-current text-[rgba(82,91,100,255)] " xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z"/></svg>
+          </button>
+          <button type="button" className="mx-1">
+            <svg className="fill-current text-[rgba(82,91,100,255)]" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M80 0v-160h800V0H80Zm160-320h56l312-311-29-29-28-28-311 312v56Zm-80 80v-170l448-447q11-11 25.5-17t30.5-6q16 0 31 6t27 18l55 56q12 11 17.5 26t5.5 31q0 15-5.5 29.5T777-687L330-240H160Zm560-504-56-56 56 56ZM608-631l-29-29-28-28 57 57Z"/></svg>
+          </button>
+        </div>
     </div>
   )
 }

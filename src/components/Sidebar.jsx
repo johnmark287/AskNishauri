403c377b91c63a382f5dd32a0831a4401a77ccf9
johnmark287 <johhnmark.muhando@students.jkuat.ac.ke>
2024-05-07 16:16:39 +0300

    feat: AskNishauri and Create chat

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,13 @@
 
 function Sidebar() {
   return (
-    <aside className="bg-[rgba(35,38,43,255)] h-screen w-[260px]">
-        Sidebar
-
+    <aside className="bg-[rgba(35,38,43,255)] h-screen w-[260px] flex flex-col text-[rgba(131,142,152,255)]">
+      <div className="text-left m-1 p-[6px] relative">
+        AskNishauri
+      </div>
+      <button type="button" className="bg-[rgba(72,81,89,255)] rounded-xl w-[90%] self-center text-white">
+        Create chat
+      </button>
     </aside>
   )
 }

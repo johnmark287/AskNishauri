a5441b76bc52c2edfb1313ff858e46e7797e32c1
johnmark287 <johhnmark.muhando@students.jkuat.ac.ke>
2024-05-08 20:17:48 +0300

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
-function Sidebar() {
+function Sidebar({open}) {
   return (
-    <aside className="bg-[rgba(35,38,43,255)] h-screen w-[260px] flex flex-col text-[rgba(131,142,152,255)]">
-      <div className="flex justify-between text-left m-1 p-[6px] relative">
-        <div className="">
+    <aside className={`${open ? "w-[260px]" : "w-[-260px]"} duration-300 bg-[rgba(35,38,43,255)] h-screen flex flex-col text-[rgba(131,142,152,255)]`}>
+      <div className="flex justify-between text-left m-1 p-[6px] relative border-b border-b-gray-500">
+        <div className="py-[2.23px]">
           AskNishauri
         </div>
-        <button type="button" className="">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+        <button type="button" onClick={() => setOpen(!open)} className=" lg:hidden pr-[1px] mr-[2px]">
+          <svg className="active:fill-current" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
         </button>
       </div>
       <button type="button" className="hover:bg-[rgba(131,142,152,255)] active:bg-[rgba(29,31,34,255)] my-2 py-[7px] bg-[rgba(72,81,89,255)] rounded-3xl w-[90%] self-center text-white">

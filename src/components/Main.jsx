1151dcea365ed371c9dce31edc8f764b0c3be7e7
johnmark287 <johhnmark.muhando@students.jkuat.ac.ke>
2024-05-09 21:05:14 +0300

    feat: add mic

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,8 +8,10 @@ function Main() {
   return (
     // main
     <div className="relative flex flex-col box-border bg-[rgba(29,31,34,255)] h-screen w-full">
-      {/* navbar comonent */}
+      {/* navbar component */}
       <Navbar />
+
+      {/* Main Component */}
       <div className="relative text-white pl-3 h-[100%] overflow-y-scroll">
         <div className="pl-[10px] flex items-center border-b border-b-[#25282d] h-[40px] py-[27px]">
           <div className="">
@@ -63,20 +65,35 @@ function Main() {
         </div>
       </div>
       {/* input */}
-      <div className="text-white flex justify-center items-center absolute bottom-0 right-0 left-0  h-20 bg-[rgba(29,31,34,255)] ">
+      <div className="text-white flex justify-center items-center bottom-0 right-0 left-0  h-[80px] bg-[rgba(29,31,34,255)] ">
         <div className="absolute bg-[#23262b] w-[69%] rounded-3xl p-2">
           <input
-            className="ml-3 break-normal break-words whitespace-nowrap outline-none bg-transparent w-[95%]"
+            className="ml-3 relative break-normal break-words whitespace-nowrap outline-none bg-transparent w-[95%]"
             type="textarea"
             name="input"
             id="input"
           />
           <button
-            type="submit"
-            className="rounded-xl absolute bottom-2 right-2 bg-[#244b42]"
+            type="button"
+            className="absolute right-[43px] active:bg-[rgba(29,31,34,255)] bg-[rgba(131,142,152,255)] bottom-[5px] rounded-2xl p-[3px]"
           >
             <svg
-              className="fill-current text-[#27c08a] p-[2px]"
+              className="hover:fill-white fill-current text-[#23262b]"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e8eaed"
+            >
+              <path d="M480-400q-50 0-85-35t-35-85v-240q0-50 35-85t85-35q50 0 85 35t35 85v240q0 50-35 85t-85 35Zm0-240Zm-40 520v-123q-104-14-172-93t-68-184h80q0 83 58.5 141.5T480-320q83 0 141.5-58.5T680-520h80q0 105-68 184t-172 93v123h-80Zm40-360q17 0 28.5-11.5T520-520v-240q0-17-11.5-28.5T480-800q-17 0-28.5 11.5T440-760v240q0 17 11.5 28.5T480-480Z" />
+            </svg>
+          </button>
+          <button
+            type="submit"
+            className="rounded-2xl absolute bottom-[5px] right-2 p-[3px] bg-[#244b42]"
+          >
+            <svg
+              className="fill-current text-[#27c08a]"
               xmlns="http://www.w3.org/2000/svg"
               height="24px"
               viewBox="0 -960 960 960"

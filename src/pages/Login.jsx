4d85030c08ff0e0e04d402b78816abe407962279
johnmark287 <johhnmark.muhando@students.jkuat.ac.ke>
2024-05-11 21:03:52 +0300

    feat: add login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+function Login() {
+  return (
+    <div className="">
+      <div className="h-screen flex justify-center items-center">
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>Basic Login Page</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-2">
+              <div className="flex flex-col items-start space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" />
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <div className="flex flex-col items-start space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" />
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Create account</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+export default Login;

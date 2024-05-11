1e8bee978667de7c6684d13ff17be546d3da45b4
johnmark287 <johhnmark.muhando@students.jkuat.ac.ke>
2024-05-11 22:45:22 +0300

    feat: delete Login File

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 
@@ -9,7 +8,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>

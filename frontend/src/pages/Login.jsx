0e224fd148f14039c452d0e92cd488847c6e8482
EphraimShikanga <ephraimshikanga@gmail.com>
2024-05-15 16:30:20 +0300

    changed email to phone in login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,11 +19,11 @@ import { Label } from "@/components/ui/label";
 
 async function handleLogin(e) {
   e.preventDefault();
-  const email = document.getElementById("phone").value;
+  const phone = document.getElementById("phone").value;
   const password = document.getElementById("password").value;
 
 
-  if (!email || !password) {
+  if (!phone || !password) {
     alert("Please fill all fields");
     return;
   }
@@ -33,7 +33,7 @@ async function handleLogin(e) {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ email: email, password: password }),
+    body: JSON.stringify({ phone: phone, password: password }),
   })
     .then((response) => response.json())
     .then((data) => {

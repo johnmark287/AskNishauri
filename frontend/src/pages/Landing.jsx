222490872af54f24e55f431becd5cf08508a6b43
johnmark287 <johhnmark.muhando@students.jkuat.ac.ke>
2024-05-29 20:23:22 +0300

    minor changes

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
-import LandingMain from "../components/LandingMain";
+import LandingSidebar from "../components/LandingSidebar";
 import LandingNavbar from "../components/LandingNavbar";
+import LandingMain from "../components/LandingMain";
+import LandingFooter from "../components/LandingFooter";
 
 function Landing() {
   const [open, setOpen] = useState(true);
@@ -10,8 +12,10 @@ function Landing() {
   }
   return (
     <div className="text-black relative">
-      <LandingNavbar open={open} setOpen={handleOpen} />
-      <LandingMain />
+      <LandingSidebar open={open} setOpen={handleOpen} />
+      <LandingNavbar setOpen={handleOpen} />
+      <LandingMain open={open}/>
+      <LandingFooter open={open}/>
     </div>
   );
 }
